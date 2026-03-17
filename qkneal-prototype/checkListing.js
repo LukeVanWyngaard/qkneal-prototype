34b4f9bb3d1e9abaeafb8c58870dff52834aad0a
LukeVanWyngaard <vanwyngaardluke@gmail.com>
2026-03-17 15:09:31 +0200

    Delete qkneal-prototype directory

--- qkneal-prototype/checkListing.js
+++
@@ -1,5 +0,0 @@
-// checkListing.js
-document.getElementById('checkBtn').addEventListener('click', () => {
-  const url = document.getElementById('urlInput').value;
-  alert("URL entered: " + url);
-});
