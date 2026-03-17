16c8a8777fad8af8e97dfcd40ec170507b895e84
LukeVanWyngaard <vanwyngaardluke@gmail.com>
2026-03-17 15:09:52 +0200

    Add checkListing.js and link to index.html
    Add checkListing.js and link to index.html

---
+++ checkListing.js
@@ -0,0 +1,5 @@
+// checkListing.js
+document.getElementById('checkBtn').addEventListener('click', () => {
+  const url = document.getElementById('urlInput').value;
+  alert("URL entered: " + url);
+});
